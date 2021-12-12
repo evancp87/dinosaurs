835af012eb868c7d4528a61ac56cddfc33bfd0c2
Evan Parker <65721884+evancp87@users.noreply.github.com>
2021-12-12 20:42:06 +0000

    dinosaur constructor classes & getter funcs

--- app.js
+++ app.js
@@ -1,25 +1,85 @@
 
     // Create Dino Constructor
+ function Dinosaur(species, diet, weight, height, where, when, fact, image)  {
+    this.species = species;
+    this.diet = diet;
+    this.weight =  weight;
+    this.height = height;
+    this.where = where;
+    this.when = when;
+    this.fact = fact;
+this.image = image;
+    }
 
 
-    // Create Dino Objects
+    // human constructor function
+
+    // function Dinosaur (species, weight, diet) 
+    // {
+        //     this.species = species;
+        //     this.weight = weight;
+        //     this.diet = diet
+        //     this.fact = function () {
+            //         console.log(`A ${this.species} likes to eat ${this.diet}`)
+            //     };
+            
+            // }
+            
+            let tRex = new Dinosaur();
+            
+            console.log(tRex);
+            // Create Dino Objects
+            
+            
+            // Create Human Object
+            
+            function Human(species, personName, diet, weight, feet, inches, image )  {
+                this.species = species;
+                this.name = personName;
+                this.diet = diet;
+                this.weight = weight;
+                this.feet = feet;
+                this.inches = inches;
+                this.image = image;
+            }
 
 
-    // Create Human Object
+            let brian = new Human();
 
-    // Use IIFE to get human data from form
+            console.log(brian);
+            // Use IIFE to get human data from form
+
+            const name = getElementById('name');
+            const feet = getElementById('feet');
+            const inches = getElementById('inches');
+            const weight = getElementById('weight');
+            const diet = getElementById('diet');
+            const button = getElementById('btn');
+
+            const grid = getElementById('grid');
 
 
-    // Create Dino Compare Method 1
+            forEach(el => {
+
+            })
+            const 
+            
+            (function () {
+                document.getElementById('btn');
+
+            })()
+            // Create Dino Compare Method 1
     // NOTE: Weight in JSON file is in lbs, height in inches. 
-
+// else if statement
     
     // Create Dino Compare Method 2
     // NOTE: Weight in JSON file is in lbs, height in inches.
+// else if statement
 
     
     // Create Dino Compare Method 3
     // NOTE: Weight in JSON file is in lbs, height in inches.
+// else if statement
 
 
     // Generate Tiles for each Dino in Array
@@ -27,6 +87,6 @@
         // Add tiles to DOM
 
     // Remove form from screen
-
+// remove method
 
 // On button click, prepare and display infographic
