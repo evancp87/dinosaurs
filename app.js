07b2f3e0d81e2756efcbf8edb3cc5d3e420b41ba
Evan Parker <65721884+evancp87@users.noreply.github.com>
2022-01-06 12:08:25 +0000

    dino tiles tweak

--- app.js
+++ app.js
@@ -84,21 +84,6 @@ class Dinosaur {
 }
 
 
-// handleError = error => {console.log(error)};
-// let dinos = [];
-// fetch("./dino.json")
-//   .then((res) => res.json())
-//   .then((data) => {
-//     let dinos = data.Dinos;
-//     console.log(dinos);
-//     return getDinos(dinos);
-//   }).catch(error => console.log(error));
-
-// function getDinos(dinos) {
-//   dinos.map((dino) => console.log(dino));
-// }
-
-
 
 
 // const getDinos = async () => {
@@ -108,7 +93,7 @@ class Dinosaur {
 //   return data;
 // }
 
-// Preferred option
+// fetch and create dino objects 
 const getDinos = fetch("./dino.json")
   .then((response) => response.json())
   .then((data) => {
@@ -131,26 +116,6 @@ const getDinos = fetch("./dino.json")
 
 
 
-// getDinos().then(data => {
-//   console.log(data)
-//   let dinos = data.forEach(dino => new Dinosaur(dino.species,
-//     dino.diet,
-//     dino.weight,
-//     dino.height,
-//     dino.where,
-//     dino.when,
-//     dino.fact,
-//     dino.image))
-// }).catch(error => {console.error(error);});
-
-// getDinos().then(data => {
-//   then((data) => {
-//     let dinos =
-//     //     let dinos = data.Dinos
-//     //     console.log(dinos);
-//     //     return dinos;
-// })
-// TODO: spread operator to concat comparisons and facts
 
 // Create Dino Objects
 
@@ -175,21 +140,18 @@ class Human {
 
 // Create Human Object
 
-// Generate Tiles for each Dino in Array
 
 // Add tiles to DOM
 
 // tileArray
 
-//TODO: can use createElement- save as a variable and then element.classList.add('your-class');
 
-// human should be index of 4
-
+// Generate Tiles for each Dino in Array
 const dinoTiles = (personData) => {
 
  
   getDinos.then(dinos => {
-    dinos.splice(0,4, personData);
+    // dinos.splice(0,4, personData);
    dinos.map(dino => { 
      
      if (dino.species === 'human') {
@@ -203,29 +165,25 @@ const dinoTiles = (personData) => {
         // return Math.floor(Math.random() * (max - min + 1) ) + min;
         //}
         
-        switch (randomFactGenerator) {
-          case 3:
-             dino.compareDiet(personData);
-            break;
-            
-            case 5:
-               dino.compareWeight(personData);
-              break;
-              case 7:
-                return dino.compareHeight(personData);
-                default:
-                   dino.fact;
-                }
-                ;
+       
+          
+          // switch (randomFactGenerator) {
+          //   case 3:
+          //      dino.compareDiet(personData);
+          //     break;
+              
+          //     case 5:
+          //        dino.compareWeight(personData);
+          //       break;
+          //       case 7:
+          //         return dino.compareHeight(personData);
+          //         default:
+          //            dino.fact;
+          //         }
+          //         ;
+  
                 
                 
-                
-                // local variable for dino data here?
-               
-                // compareWeight(personObject);
-                // compareHeight(personObject);
-                // compareDiet(personObject);
-                // randomFacts(personObject);
                 
                 
                 // TODO: remember to change this
@@ -233,9 +191,9 @@ const dinoTiles = (personData) => {
                 const dinoGrid = document.getElementById("grid");
                 const dinoTile = document.createElement("div");
                 dinoTile.classList.add("grid-item");
-                const dinoImg = `../images/${dino.image}.png}`;                
+                
                 dinoTile.innerHTML = `<h3>${dino.species}</h3>
-                <img src='${dinoImg}'>
+                <img src='../images/${dino.image}.png}' alt='${dino.species} image'>
                
                 <p>${dino.fact}</p>`;
                 dinoGrid.appendChild(dinoTile);
@@ -252,8 +210,8 @@ const dinoTiles = (personData) => {
                 
                 // humanTile.innerHTML(`<h3>${humanTile.name}</h3>
                 // <img src='images/${humanTile}.png'`);
-                // dinoArray.splice(4, 0, humanTile);
                 dinoGrid.appendChild(dinoTile);
+                // dinoGrid.splice(4, 0, humanTile);
                 // tileArray.push(dinoArrays);
                 // const allTiles = [...tileArray, ...humanTile];
                 // console.log(allTiles);
@@ -263,91 +221,7 @@ const dinoTiles = (personData) => {
             };
             
             
-            
-           
-
-
-
-
-
-
-            
-// const dinoTiles =  async (personData) => {
-
-//   const dinos = await getDinos();
-//   getDinos().then(dinos => {
-//     dinos.splice(0,4, personData);
-//    dinos.map(dino => 
-//     {
-//       if (dino.species === 'human') {
-//         return dino.species = personData.name;
-//       } else if (dino.species === 'pigeon') {
-//        return dino.fact = 'All dinosaurs are living dinosaurs';
-//       }
-      
-
-//         let randomFactGenerator = Math.floor(Math.random() * 10);
-//         // function randomNum(min, max) {
-//        // return Math.floor(Math.random() * (max - min + 1) ) + min;
-//       //}
-    
-//         switch (randomFactGenerator) {
-//           case 3:
-//             return dino.compareDiet(personData);
-//             break;
-    
-//           case 5:
-//             return dino.compareWeight(personData);
-//     break;
-//           case 7:
-//             return dino.compareHeight(personData);
-//     break;
-//           default:
-//             return dino.fact;
-//         }
-//       ;
-
-//     }
-    
-//     ).catch(error => console.log(error));
-//   })
-//   // local variable for dino data here?
-//   let tileArray;
-//   // compareWeight(personObject);
-//   // compareHeight(personObject);
-//   // compareDiet(personObject);
-//   // randomFacts(personObject);
-
-
-//   // TODO: remember to change this
-
-//     const dinoGrid = getElementById("grid");
-//     const dinoTile = document.createElement("div");
-//     dinoTile.classList.add("grid-item");
-//     const dinoImg = `../images/${dino.img}.png}`;
-//     // let element = document.createElement("div");
-
-//     dinoTile.innerHTML = `<h3>${dino.species}</h3>
-//     <img src='images/${dinoImg.species}.png'>
-//     <p>${dinoArray.fact}</p>`;
-//     dinoGrid.appendChild(dinoTile);
-
-//     if (dino.species === "pigeon") {
-//       dinoTile.innerHTML(`<p>All birds are dinosaurs</p>`);
-//     }
-
-//     const humanTile = document.createElement("div");
-//     humanTile.classList.add("grid-item");
-
-//     humanTile.innerHTML(`<h3>${humanTile.name}</h3>
-//   <img src='images/${human}.png'`);
-//     // dinoArray.splice(4, 0, humanTile);
-//     dinoGrid.appendChild(dinoTile);
-//     // tileArray.push(dinoArrays);
-//     // const allTiles = [...tileArray, ...humanTile];
-//     // console.log(allTiles);
-  
-// };
+// Get human data from form 
 
 function getPersonData() {
   const personName = document.getElementById("name").value;
@@ -365,60 +239,11 @@ function getPersonData() {
 
 // TODO: spread operator let tiles = [...dinos, ...human]; or let tiles = dinos.concat.(human);
 
-// remove method
+
 
 // On button click, prepare and display infographic
-
-// let btn = document.getElementById("btn");
-// btn.addEventListener("click", (e) => {
-//   e.preventDefault();
-//   let removeForm = () => {
-//     const formRemove = (document.getElementById("dino-compare").style.display =
-//     "none");
-//   };
-//   removeForm();
-//   const displayTiles = () => {
-//   const showGrid = document.getElementById("grid").style.display = "block";
-//   // dinoTiles();
-//   };
-
-//   getPerson();
-//   displayTiles();
-// });
-
-// const dinoData = fetch("./dino.json")
-//   .then((response) => response.json())
-//   .then((data) => {
-//     console.log(data)
-//     let dinos = data.Dinos.map((dino) =>
-//     new Dinosaur(
-//       dino.species,
-//       dino.weight,
-//       dino.height,
-//       dino.diet,
-//       dino.where,
-//       dino.when,
-//       dino.fact, dino.image
-//       ).catch(error => console.log(error))
-
-//       )
 document.getElementById("btn").addEventListener("click", (e) => {
-  //       const dinoData = fetch("./dino.json")
-  // .then((response) => response.json())
-  // .then((data) => {
-  //   console.log(data)
-  //   let dinos = data.Dinos.map((dino) =>
-  //   new Dinosaur(
-  //     dino.species,
-  //     dino.weight,
-  //     dino.height,
-  //     dino.diet,
-  //     dino.where,
-  //     dino.when,
-  //     dino.fact, dino.image
-  //     ).catch(error => console.log(error))
-
-  //     )
+  
   e.preventDefault();
   let removeForm = () => {
     const formRemove = (document.getElementById("dino-compare").style.display =
@@ -428,14 +253,6 @@ document.getElementById("btn").addEventListener("click", (e) => {
   
   const human = getPersonData();
   dinoTiles(human);
-  // const displayTiles = () => {
-  //   const showGrid = (document.getElementById("grid").style.display = "block");
-  //   showGrid();
-  // };
-
-  // const humanData = new Human(name.value, feet.value, inches.value, diet.value);
-  // displayTiles();
 });
 
 
-// })
